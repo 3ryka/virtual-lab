b090f21c17d21aa3da27ed370fa19915ca0545bd
3ryka <saltedpopcorn48@gmail.com>
2024-10-24 19:13:12 +0700

    upload files

---
+++ script.js
@@ -0,0 +1,21 @@
+/*window.onscroll = function() {
+    const header = document.querySelector('.header');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+};*/
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navmenu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".navlink").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
